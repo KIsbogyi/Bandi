c88789c1cde901a5a6bf23b0b0cef13f11d25559
KIsbogyi <55801726+KIsbogyi@users.noreply.github.com>
2019-11-11 09:21:07 +0100

    Add files via upload

---
+++ robi a robot.py
@@ -0,0 +1,64 @@
+import math
+lst=[]
+def F1():
+    print('beolvasom a programot pls stand by')
+    fbe=open('program.txt', 'r')
+    global p
+    p=int(fbe.readline())
+    for k in fbe:
+        lst.append(k.strip())
+    print(lst)
+    fbe.close()
+
+def F2():
+    usz=int(input('kérem egy utasítás sorszámát'))
+    print('vizsgálat tárgya:',lst[usz-1])
+    prg=lst[usz-1]
+    van=False
+    nincs=True
+    i=0
+    h=set()
+    h={'ED', 'DE' ,'KN', 'NK'}
+    while i<len(prg)-1 and nincs:
+        s=prg[i]+prg[i+1]
+        if s in h:
+            nincs=False
+            break
+        i+=1
+    if nincs:
+        print('nem egyszerűsithető')
+    else:
+        print('egyszerűsithető')
+
+
+
+    x=0
+    y=0
+    maxi_tav=0
+    lepes=0
+    for i in range(len(prg)):
+        if prg[i]=='E':
+            y+=1
+        if prg[i]=='D':
+            y-=1
+        if prg[i]=='N':
+            x+=1
+        if prg[i]=='K':
+            x-=1
+        c=math.sqrt(x**2+y**2)
+        if c>maxi_tav:
+            maxi_tav=c
+            lepes=i+1
+
+    print(abs(y),'lépést kell tenni x-tengelyen')
+    print(abs(x),'lépést kell tenni y-tengelyen')
+    print('a legnagyobb táv a %d. lépésben: %.3f' %(lepes,maxi_tav))
+            
+        
+        
+
+    
+#g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2g2
+F1()
+F2()
+
